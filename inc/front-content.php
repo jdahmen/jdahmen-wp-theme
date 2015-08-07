43930ecbb2e39e6cd6f5d5e2c42e27f04f44f6c8
Jordan Dahmen <jordandahmen@gmail.com>
2015-08-07 02:12:10 -0400

    Added logo image

--- inc/front-content.php
+++ inc/front-content.php
@@ -115,7 +115,7 @@
       <h1><a href="<?php echo bloginfo('url') . '/contact/'; ?>">Contact</a></h1>
       <div class="row">
         <div class="col-md-12">
-          <?php echo do_shortcode( '[contact-form-7 id="57" title="Home contact form"]' ); ?>
+          <?php echo do_shortcode( '[contact-form-7 id="36" title="Home contact form"]' ); ?>
         </div>
       </div>
     </div>
