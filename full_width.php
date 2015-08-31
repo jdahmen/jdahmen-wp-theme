a9aae59428a6ba412eb4a479e9563e683f0dd082
Jordan Dahmen <jordandahmen@gmail.com>
2015-08-30 21:38:29 -0400

    Added full width page template

---
+++ full_width.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * A full width page template file.
+ *
+ * @package JDahmen
+ * @subpackage JDahmen_Theme
+ * 
+ * Template Name: Full Width 
+ */
+
+// Gets header.php
+get_header(); ?>
+	 
+	<div class="container-fluid">
+		<?php include 'inc/display-single.php'; ?>
+	</div>
+
+
+<?php // Gets footer.php
+	get_footer(); ?>
