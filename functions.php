c2ed7b9e502a926838ebb3fe3bdac2ec5389f452
Jordan Dahmen <jordandahmen@gmail.com>
2015-08-30 21:47:33 -0400

    Finished implementing support of newer form theme/design in comment section

--- functions.php
+++ functions.php
@@ -1,11 +1,12 @@
 <?php 
 
-// Adapted from https://gist.github.com/toscho/1584783
+/* Adapted from https://gist.github.com/toscho/1584783 */
 add_filter( 'clean_url', function( $url )
 {
     if ( FALSE === strpos( $url, '.js' ) )
-    { // not our file
-        return $url;
+    { 
+      // not our file
+      return $url;
     }
     // Must be a ', not "!
     return "$url' defer='defer";
@@ -40,5 +41,71 @@ add_theme_support( 'post-thumbnails' );
 add_action( 'after_setup_theme', 'cwd_wp_bootstrapwp_theme_setup' );
 
 require_once 'inc/nav.php';
+
+function custom_comments ($comment, $args, $depth) {
+  $GLOBALS['comment'] = $comment; ?>
+    <li <?php comment_class('col-md-12'); ?> id="comment-li-<?php comment_ID(); ?>">
+      <div class="row">
+        <div class="col-md-1 col-xs-2 comment-avatar">
+          <?php echo get_avatar($comment, $size='256', $default='mystery'); ?>
+        </div>
+        <div class="col-md-11 col-xs-10">
+          <div class="row comment-content">
+            <div class="col-md-12 comment-author vcard">   
+              <?php printf(__('<cite class="fn">%s</cite> '),  get_comment_author( $comment_ID )) ?>
+              <div class="comment-meta">
+                <?php printf(__('%1$s at %2$s'), get_comment_date(), get_comment_time()) ?>
+                <?php edit_comment_link(__('(Edit)'),'  ','') ?>
+              </div>
+            </div>
+
+            <?php if ($comment->comment_approved == '0') : ?>              
+              <div class="col-md-12">
+                <em><?php _e('Your comment is awaiting moderation.'); ?></em>
+                <br />
+              </div>
+            <?php endif; ?>
+
+            <div class="col-md-12 text">
+              <?php comment_text(); ?>
+            </div>
+
+            <div class="col-md-12">
+              <div class="reply">
+                <?php comment_reply_link(array_merge( $args, array('depth' => $depth, 'max_depth' => $args['max_depth']))) ?>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    <?php
+}
+
+/* Comment submission form customization */
+
+function alter_comment_form_fields($fields){
+    
+  $fields = array(
+    'author' =>
+      '<div class="row"><div class="col-md-6 comment-form-author "><div class="action-wrapper">' . 
+      '<input id="author" name="author" type="text" value="' . esc_attr( $commenter['comment_author'] ) .
+      '" size="30"' . $aria_req . ' />' . 
+      '<label for="author">' . __( 'Name', 'domainreference' ) . '</label> ' .
+      '</div></div>',
+
+    'email' =>
+      '<div class="col-md-6 comment-form-email"><div class="action-wrapper">' . 
+      '<input id="email" name="email" type="text" value="' . esc_attr(  $commenter['comment_author_email'] ) .
+      '" size="30"' . $aria_req . ' />' .       
+      '<label for="email">' . __( 'Email', 'domainreference' ) . '</label> ' .
+      '</div></div></div>',
+
+    'url' => '',
+  );
+
+  return $fields;
+}
+
+add_filter('comment_form_default_fields','alter_comment_form_fields');
 
 ?>
